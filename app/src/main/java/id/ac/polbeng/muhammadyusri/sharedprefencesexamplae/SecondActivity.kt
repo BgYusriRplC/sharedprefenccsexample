715e188e4bddaf04d4b3e1620aa0b1acdfde61d8
lenovo <yusrikualiitam@gmail.com>
2024-11-11 08:02:01 -0800

    shareprefence.kt

---
+++ app/src/main/java/id/ac/polbeng/muhammadyusri/sharedprefencesexamplae/SecondActivity.kt
@@ -0,0 +1,29 @@
+package id.ac.polbeng.muhammadyusri.sharedprefencesexamplae
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import id.ac.polbeng.muhammadyusri.sharedprefencesexamplae.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLoad.setOnClickListener {
+            val filename = "$packageName-${MainActivity.RPL}"
+            val pref = getSharedPreferences(filename, Context.MODE_PRIVATE)
+            val firstName = pref.getString("firstName", "")
+            val lastName = pref.getString("lastName", "")
+            binding.tvOutput.text = "$firstName $lastName "
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        binding.tvOutput.text = "Click Button Load Data"
+    }
+}
